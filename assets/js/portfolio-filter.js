2c86b682475581135776efd42d8c27fbd23f48bf
Avinash Mishra <avimshra@gmail.com>
2021-05-10 17:56:57 +0545

    final

--- assets/js/portfolio-filter.js
+++ assets/js/portfolio-filter.js
@@ -2,6 +2,52 @@
 
 var previousClickedMenuLink = undefined;
 
+$('#akku_modal').on('click', function (e) {
+  var cld = cloudinary.Cloudinary.new({ cloud_name: 'due5usvkm' });
+  var demoplayer = cld.videoPlayer('doc-player').width(600);
+  demoplayer.source(
+    'https://res.cloudinary.com/azwraith/video/upload/v1620644035/akku_n50rcv.mp4'
+  );
+});
+
+$('#infytrip_modal').on('click', function (e) {
+  var cld = cloudinary.Cloudinary.new({ cloud_name: 'due5usvkm' });
+  var demoplayer = cld.videoPlayer('infytrip-client-player').width(600);
+  demoplayer.source(
+    'https://res.cloudinary.com/azwraith/video/upload/v1620644035/infytrip_y1micy.mp4'
+  );
+
+  var cld2 = cloudinary.Cloudinary.new({ cloud_name: 'due5usvkm' });
+  var demoplayer2 = cld2.videoPlayer('infytrip-dashboard-player').width(600);
+  demoplayer2.source(
+    'https://res.cloudinary.com/azwraith/video/upload/v1620643966/infytrip_dashboard_fk3hrz.mp4'
+  );
+});
+
+$('#kunyov2_modal').on('click', function (e) {
+  var cld = cloudinary.Cloudinary.new({ cloud_name: 'due5usvkm' });
+  var demoplayer = cld.videoPlayer('kunyov2-player').width(600);
+  demoplayer.source(
+    'https://res.cloudinary.com/azwraith/video/upload/v1620644013/kunyov2_qzeq5y.mp4'
+  );
+});
+
+$('#marketing_modal').on('click', function (e) {
+  var cld = cloudinary.Cloudinary.new({ cloud_name: 'due5usvkm' });
+  var demoplayer = cld.videoPlayer('marketing-player').width(600);
+  demoplayer.source(
+    'https://res.cloudinary.com/azwraith/video/upload/v1620643989/kunyomarketing_dnr0vm.mp4'
+  );
+});
+
+$('#logistics_modal').on('click', function (e) {
+  var cld = cloudinary.Cloudinary.new({ cloud_name: 'due5usvkm' });
+  var demoplayer = cld.videoPlayer('logistics-player').width(600);
+  demoplayer.source(
+    'https://res.cloudinary.com/azwraith/video/upload/v1620644141/Kunyo_logistics_qvb4se.mp4'
+  );
+});
+
 $(document).on('ready', function (event) {
   console.log('ready');
 

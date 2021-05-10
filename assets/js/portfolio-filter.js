63eef08a3eec0f42150e07899faa17f86fefc5e4
Avinash Mishra <avimshra@gmail.com>
2021-05-10 14:30:00 +0545

    new updates

--- assets/js/portfolio-filter.js
+++ assets/js/portfolio-filter.js
@@ -1,30 +1,52 @@
 'use strict';
 
-//Filter project cards
 var previousClickedMenuLink = undefined;
-$('.portfolio-menu').on('click', 'a', function(event){
-    event.preventDefault();
 
-    if (previousClickedMenuLink) {
-        previousClickedMenuLink.removeClass('portfolio-menu__link--active');
+$(document).on('ready', function (event) {
+  console.log('ready');
+
+  var portfolioItems = $('.portfolio-cards').children();
+  console.log({ portfolioItems });
+
+  portfolioItems.hide();
+
+  portfolioItems.each(function (index, value) {
+    var item = $(value);
+    if (item.data('portfolio-tag') === 'web-sites') {
+      item.fadeIn({ duration: 500 });
     }
-    var link = $(event.target);
-    link.addClass('portfolio-menu__link--active');
-    previousClickedMenuLink = link;
+  });
 
-    var targetTag = $(event.target).data('portfolio-target-tag');
-    var portfolioItems = $('.portfolio-cards').children();
+  previousClickedMenuLink = $('#web_sites_href');
 
-    if (targetTag === 'all') {
-        portfolioItems.fadeIn({duration: 500});
-    } else {
-        portfolioItems.hide();
+  $('#web_sites_href').addClass('portfolio-menu__link--active');
+});
+//Filter project cards
+$('.portfolio-menu').on('click', 'a', function (event) {
+  event.preventDefault();
+
+  if (previousClickedMenuLink) {
+    previousClickedMenuLink.removeClass('portfolio-menu__link--active');
+  }
+  var link = $(event.target);
+  link.addClass('portfolio-menu__link--active');
+  previousClickedMenuLink = link;
+
+  var targetTag = $(event.target).data('portfolio-target-tag');
+  var portfolioItems = $('.portfolio-cards').children();
+
+  console.log({ targetTag });
+
+  if (targetTag === 'all') {
+    portfolioItems.fadeIn({ duration: 500 });
+  } else {
+    portfolioItems.hide();
+  }
+
+  portfolioItems.each(function (index, value) {
+    var item = $(value);
+    if (item.data('portfolio-tag') === targetTag) {
+      item.fadeIn({ duration: 500 });
     }
-
-    portfolioItems.each(function(index, value){
-        var item = $(value);
-        if (item.data('portfolio-tag') === targetTag) {
-            item.fadeIn({duration: 500});
-        }
-    });
+  });
 });
